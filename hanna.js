3f916b685fb5096c2003dce8375385db1f1592c5
Hanna Ostlund <143076946+hannaostlund@users.noreply.github.com>
2023-11-17 12:20:12 +0100

    Update hanna.js

--- hanna.js
+++ hanna.js
@@ -1,7 +1,4 @@
 console.log("Hi my name is Hanna");
 console.log("My hobbies are reading and collecting vintage designer handbags"); 
-<<<<<<< HEAD
-
-=======
 console.log("Västerås");
->>>>>>> main
+console.log("My age is 33");
